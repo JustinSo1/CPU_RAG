4fe71c376bb27fd6809c4abd228c2959c27d2412
Justin So <sojustin28@gmail.com>
2025-01-03 16:15:53 -0500

    Remove case insensitive dir

--- TARGET/target_benchmark/generators/GeneratorsDataModels.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Tuple, Union
-
-from pydantic import BaseModel
-
-
-class DownstreamGeneratedResultDataModel(BaseModel):
-    dataset_name: str
-    query_id: Union[int, str]
-    generated_results: Union[str, Tuple[str, str], List[str]]
